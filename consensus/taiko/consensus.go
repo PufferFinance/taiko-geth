d11080fa7f2d16286f247de4ce523b1be4f23ae5
failfmi <oscurocalma@gmail.com>
2024-09-11 16:40:34 +0300

    feat(anchor-tx): extend function selector to include deposits root

--- consensus/taiko/consensus.go
+++ consensus/taiko/consensus.go
@@ -29,9 +29,9 @@ var (
 
 	GoldenTouchAccount   = common.HexToAddress("0x0000777735367b36bC9B61C50022d9D0700dB4Ec")
 	TaikoL2AddressSuffix = "10001"
-	AnchorSelector       = crypto.Keccak256([]byte("anchor(bytes32,bytes32,uint64,uint32)"))[:4]
+	AnchorSelector       = crypto.Keccak256([]byte("anchor(bytes32,bytes32,bytes32,uint64,uint32)"))[:4]
 	AnchorV2Selector     = crypto.Keccak256(
-		[]byte("anchorV2(uint64,bytes32,uint32,(uint8,uint8,uint32,uint64,uint32))"),
+		[]byte("anchorV2(uint64,bytes32,bytes32,uint32,(uint8,uint8,uint32,uint64,uint32))"),
 	)[:4]
 	AnchorGasLimit = uint64(250_000)
 )
